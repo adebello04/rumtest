99ca4b220e1e66853cd43d91d1d1be8ed0b12a60
adebello04 <53411077+adebello04@users.noreply.github.com>
2021-01-23 22:51:58 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Telegraf = require('telegraf');
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || '8080'
+
+
+
+
+const bot = new Telegraf("1473534811:AAGJoD2Vdp6aCTwz2PklMEQn-Qx-G2VspIk")
+// Set the bot response
+bot.on('text', ({ replyWithHTML}) => replyWithHTML('<b>Hello</b>'))
+
+bot.telegram.setWebhook('https://runtest-13025.nodechef.com/secret-path')
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+// Set the bot API endpoint
+app.use(bot.webhookCallback('/secret-path'))
+app.listen(PORT, () => {
+  console.log('Example app listening on port '+PORT+'!')
+})
