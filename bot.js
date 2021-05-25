b0f9608f19a547dff6b3e23391547ddf0f7555d3
slowworks <53411077+slowworks@users.noreply.github.com>
2021-05-25 16:02:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 // Set the bot response
 bot.on('text', ({ replyWithHTML}) => replyWithHTML('<b>Hello</b>'))
 
-bot.telegram.setWebhook(process.env.URL+'/secretpath')
+bot.telegram.setWebhook(process.env.URL+'/secret-path')
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
