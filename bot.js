113aad6382b708ec88153fd0befb25c37bb7e2bb
slowworks <53411077+slowworks@users.noreply.github.com>
2021-05-25 16:06:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,16 +8,16 @@ const PORT = process.env.PORT || '8080'
 
 
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+/*const bot = new Telegraf(process.env.BOT_TOKEN)
 // Set the bot response
 bot.on('text', ({ replyWithHTML}) => replyWithHTML('<b>Hello</b>'))
 
 bot.telegram.setWebhook(process.env.URL+'/secret-path')
 
-
+*/
 app.get('/', (req, res) => res.send('Hello World!'))
 // Set the bot API endpoint
-app.use(bot.webhookCallback('/secret-path'))
+//app.use(bot.webhookCallback('/secret-path'))
 app.listen(PORT, () => {
   console.log('Example app listening on port '+PORT+'!')
 })
